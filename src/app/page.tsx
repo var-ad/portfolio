9839c6e3b60a432ac697e3d485ca0a95f8ab8955
vichstein <varad.chaskar.27@gmail.com>
2025-09-20 01:26:04 +0530

    125am20sept

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
 // app/page.tsx
 export default function Home() {
-  return <h1>Hey, I am Varad.</h1>;
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-6">
+      <h1 className="text-3xl font-bold mb-6">Hey, I am Varad.</h1>
+      <img 
+        src="/github-snake-dark.svg" 
+        alt="Snake animation" 
+        width={1000} 
+        height={1000} 
+      />
+    </main>
+  );
 }
