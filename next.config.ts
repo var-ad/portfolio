42fb3ee316d04b3b30b5b22a6f05e1495252dcbc
var-ad <varad.chaskar.27@gmail.com>
2026-04-17 03:38:02 +0530

    fix: infinite redirect

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.varad.fyi" }],
-        destination: "https://varad.fyi/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
